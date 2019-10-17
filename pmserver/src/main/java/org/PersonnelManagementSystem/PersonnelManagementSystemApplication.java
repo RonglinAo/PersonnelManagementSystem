a5e523b207776a135b832101784fcf621f260bf8
Ronglin.A <ronglinao@gmail.com>
2019-10-17 17:13:19 +0800

    增加了后端 Spring Boot 的 Maven 项目，使用     Spring Boot 2.2.0     JDK 1.8.0_221     Maven 3.6.2

---
+++ pmserver/src/main/java/org/PersonnelManagementSystem/PersonnelManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.PersonnelManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonnelManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonnelManagementSystemApplication.class, args);
+	}
+
+}
---
+++ pmserver/src/test/java/org/PersonnelManagementSystem/PersonnelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.PersonnelManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonnelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

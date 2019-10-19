cc2d5d7fec32fe263d26eab0bc9f8d84068da26a
Ronglin.A <ronglinao@gmail.com>
2019-10-19 09:27:43 +0800

    添加了Swagger 依赖，添加了员工信息的InfoController。

---
+++ pmserver/src/main/java/org/PersonnelManagementSystem/bean/Employee.java
@@ -0,0 +1,10 @@
+package org.PersonnelManagementSystem.bean;
+
+/**
+ * @program: PersonnelManagementSystem
+ * @description: Employee bean
+ * @author: Ronglin.A
+ * @create: 2019-10-18 17:14
+ */
+public class Employee {
+}
